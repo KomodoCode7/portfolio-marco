08ceff4e313e1e1fe8aaac2e1479a9734f5af35e
CiervoZombie <marcoxcolo@gmail.com>
2024-11-03 19:51:03 -0300

    comit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ['@fontsource-variable/montserrat'] // Especifica el paquete que necesita ser excluido del procesamiento en SSR
+    }
+  }
+});
